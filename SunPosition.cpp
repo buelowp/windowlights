65854ecdcb204958e47ccf578698c8ad032ab6fe
Peter Buelow <goballstate@gmail.com>
2015-02-09 12:38:44 -0600

    Licensing added. Notes about where SunPosition came from. Not much code change though.

--- SunPosition.cpp
+++ SunPosition.cpp
@@ -1,3 +1,27 @@
+/*
+    This file is part of WindowLights.
+
+    SunPosition is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SunPosition is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    
+    This implementation is taken and modified from Mike Chirico at
+    http://souptonuts.sourceforge.net/code/sunrise.c.html
+    
+    Modifications done have been in the spirit of the GPL, and
+    any copying or reuse of this header and class are subject to
+    the GPL v2 or any later GPL at the users discretion.
+    All thanks for this work go to Mike who made it pretty easy.
+*/
 #include "SunPosition.h"
 
 SunPostion::SunPosition()
